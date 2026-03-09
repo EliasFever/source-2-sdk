ac0531b8d4d3dd60498fc793898e5b48c9ac3298
Roman <romanplay@mail.ru>
2026-03-09 19:26:22 +0300

    remove base and tools override, we didnt need them for a while now

--- game/addons/tools_override/Code/Editor/RectEditor/Settings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Editor.RectEditor;
-
-public class Settings
-{
-	[Hide] public string ReferenceMaterial { get; set; } = null;
-	[Hide] public bool ShowNormalizedValues { get; set; } = false;
-	public int GridSize { get; set; } = 16;
-}
