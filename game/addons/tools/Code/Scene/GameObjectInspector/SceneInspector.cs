31b542ff89dc3ded5cfddf26724eae98bfb834b7
JohnFreeman <hljohnfreeman@gmail.com>
2026-03-10 17:28:26 +0200

    Update SceneInspector
    - Reuse SceneInspector to hold more useful info. I really disliked how you were forced to create SceneInformation component yourself to store stuff for the scene that seem unecessary to be done manually all the time, like tags. So now it creates a hidden component itself and updates the relevant stuff, syncs with the inspector. Make sure to save, so it well saves... It's metadata after all! In the future we'll have your own component and/or make this extendable be replaceable with own derivative, there's defn stuff we can store there like chapters, previe images, etc etc etc. A bunch of stuff that can be checked prior to loading that scene, so we'll get around to that one day.
    Disclaimer: the component doesn't exist until you actually accessed the inspector yourself.
    - Added a bunch of stuff also that you'd see as part of GameManager params, seemed really far away and not super comfy to access before, now we're embedding it here (its extendable per project). You can modify this before and during play, essentially much nicer and mimics how we used to do this before switching to gameobjectsystems. Feel free to add to this via HL2KSceneInspectorExtension in editor.
    
    https://cdn.discordapp.com/attachments/572290095873261595/1480949753415073914/image.png

--- game/addons/tools/Code/Scene/GameObjectInspector/SceneInspector.cs
+++ game/addons/tools/Code/Scene/GameObjectInspector/SceneInspector.cs
@@ -1,18 +1,60 @@
 ﻿namespace Editor.Inspectors;
 
+public interface ISceneInspectorExtension
+{
+	void Extend( Layout layout, SerializedObject sceneSo );
+}
+
 [Inspector( typeof( Scene ) )]
 public class SceneInspector : InspectorWidget
 {
+	public static List<ISceneInspectorExtension> Extensions { get; } = [];
+
+	private SceneInformation GetOrCreateInfo()
+	{
+		var scene = SerializedObject.Targets.OfType<Scene>().FirstOrDefault();
+		if ( scene == null ) return null;
+
+		var info = scene.GetAllComponents<SceneInformation>().FirstOrDefault();
+		if ( info == null )
+		{
+			var go = scene.CreateObject();
+			go.Name = "Scene Metadata";
+			go.Tags.Add( "scene_metadata" );
+			go.Flags |= GameObjectFlags.Hidden;
+			info = go.Components.Create<SceneInformation>();
+		}
+		return info;
+	}
+
 	public SceneInspector( SerializedObject so ) : base( so )
 	{
+		var info = GetOrCreateInfo();
+		if ( info == null ) return;
+
+		var infoSo = EditorTypeLibrary.GetSerializedObject( info );
+
 		var cs = new ControlSheet();
 		cs.Margin = 8;
+		cs.AddRow( so.GetProperty( nameof( Scene.TimeScale ) ) );
+		cs.AddRow( so.GetProperty( nameof( Scene.WantsSystemScene ) ) );
 
-		cs.AddRow( SerializedObject.GetProperty( nameof( Scene.TimeScale ) ) );
-		cs.AddRow( SerializedObject.GetProperty( nameof( Scene.WantsSystemScene ) ) );
+		var metaLabel = new Label( "Metadata" );
+		metaLabel.SetStyles( "font-weight: bold; padding: 5px 8px;" );
+
+		var csMeta = new ControlSheet();
+		csMeta.Margin = 8;
+		csMeta.AddRow( infoSo.GetProperty( nameof( SceneInformation.Title ) ) );
+		csMeta.AddRow( infoSo.GetProperty( nameof( SceneInformation.SceneTags ) ) );
 
 		Layout = Layout.Column();
 		Layout.Add( cs );
+		Layout.Add( metaLabel );
+		Layout.Add( csMeta );
+
+		foreach ( var ext in Extensions )
+    		ext.Extend( Layout, so );
+
 		Layout.AddStretchCell();
 	}
 }
