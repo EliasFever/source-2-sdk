fd9edaaee334cee1dc9a6e5be09b81bdca45712d
JohnFreeman <hljohnfreeman@gmail.com>
2026-04-23 03:40:17 +0300

    Big commit pushing most of the DevUI code
    Pushing this first, this commit has a bunch of code in it aimted at mostly implementing everything S&Box had for dev ui that was handled in menu addon, but now it's in Base addon instead.
    
    We're solving a few things here ourselves however, of note worthy:
    - DevWindow is a new way of handling debug/developer centric windows. They're expected to be draggable windows that can be scaled, enriched with any content one would want. This is influenced by XGUI library, but we're doing it natively in Base addon instead now.
    - Console and loading screens are made in a way where they're expected to work in standalone too.
    - Console has better autocomplete, let's you optionally hide full stack trace.
    - They are also user configurable and extendable. More on loading screens later, but on console extension specifically we've implemented IDevUITab interfacing:
    This allows us to let any project to extend the console with new tabs that can drive any logic for debug purposes mainly. Can be extremely useful in standalone specifically without needing for the developers to implement their own imgui esque debug. Can easily scale and have more tabs if you wish.
    
    https://cdn.discordapp.com/attachments/572290095873261595/1496665239641653248/image.png
    https://cdn.discordapp.com/attachments/572290095873261595/1496665546114994258/image.png
    https://cdn.discordapp.com/attachments/572290095873261595/1496665612200448000/image.png
    
    https://cdn.discordapp.com/attachments/1066716025036492820/1492885311422861422/image.png
    https://cdn.discordapp.com/attachments/1066716025036492820/1492969927878119554/image.png

---
+++ game/addons/base/code/UISystem/DevUI/DevUI.cs
@@ -0,0 +1,92 @@
+﻿namespace Sandbox.UI.Dev;
+
+using Sandbox;
+
+public class DevLayer : RootPanel
+{
+	ExceptionNotification ExceptionNotification;
+
+	public DevLayer()
+	{
+		Log.Info( "Creating DevLayer" );
+
+		// Stylesheet autoload depends on TypeLibrary metadata (ClassFileLocationAttribute),
+		// which can be missing in some base-context init paths. Load the key DevUI sheets
+		// explicitly so the UI is visible even without TypeLibrary enrollment.
+		LoadDevUiStyles();
+
+		AddChild<DeveloperMode>();
+		AddChild<ConsoleOverlay>();
+
+		ExceptionNotification = AddChild<ExceptionNotification>();
+
+		MenuUtility.AddLogger( OnConsoleMessage );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		// DevUI.cs.scss disables pointer events for the whole DevLayer unless this class is present.
+		// Keep it in sync with the devui open state so we never end up with a visible-but-non-interactive UI.
+		SetClass( "developermode", DeveloperMode.Open );
+	}
+
+	void LoadDevUiStyles()
+	{
+		const bool failSilently = true;
+
+		// Use non-rooted paths here so we don't end up loading the same sheet twice
+		// (auto-loaded sheets are typically non-rooted via ClassFileLocationAttribute).
+		StyleSheet.Load( "UISystem/DevUI/DevUI.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Window/DevWindow.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Tabs/DevTabs.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/DevMode/DeveloperMode.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/DevMode/ConvarToggle.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/DevMode/RenderModeSelect.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Stats/StatsContainer.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Stats/StatValue.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/ConsoleOverlay/ConsoleOverlay.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Console/Console.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Console/ConsoleRow.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Console/LogEventPanel.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Layouts/DevGroup.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Layouts/Columns.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Layouts/Row.razor.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Controls/DevScrollPanel.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Controls/DevScrollBar.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Controls/DevScrollView.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/Controls/DevVirtualList.cs.scss", true, failSilently );
+		StyleSheet.Load( "UISystem/DevUI/ExceptionNotification.cs.scss", true, failSilently );
+
+		// Generic UI controls used by DevUI extension tabs.
+		StyleSheet.Load( "UI/Controls/VideoPanel.razor.scss", true, failSilently );
+		StyleSheet.Load( "UI/Controls/VideoControls.razor.scss", true, failSilently );
+	}
+
+	public override void OnDeleted()
+	{
+		base.OnDeleted();
+
+		MenuUtility.RemoveLogger( OnConsoleMessage );
+	}
+
+	[ConVar( "devui_scale" )]
+	public static float DevUI_Scale { get; set; } = 1.0f;
+
+	protected override void UpdateScale( Rect screenSize )
+	{
+		Scale = Screen.DesktopScale * DevUI_Scale;
+	}
+
+	private void OnConsoleMessage( LogEvent entry )
+	{
+		if ( !ThreadSafe.IsMainThread )
+			return;
+
+		if ( entry.Level == LogLevel.Error )
+		{
+			ExceptionNotification.OnException( entry );
+		}
+	}
+}
