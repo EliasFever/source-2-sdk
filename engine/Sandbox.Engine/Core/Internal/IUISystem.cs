3cd32b878ad3b3662c358782f6db4033dd055b9e
Roman <romanplay@mail.ru>
2026-05-02 04:36:10 +0300

    make the game boot because im stupid

--- engine/Sandbox.Engine/Core/Internal/IUISystem.cs
+++ engine/Sandbox.Engine/Core/Internal/IUISystem.cs
@@ -44,15 +44,15 @@ public interface IUISystem
 	//	Current?.Popup( "error", title, subtitle );
 	//}
 
-	/// <summary>
-	/// The backend is telling us that the number of users playing has changed
-	/// </summary>
-	void PackageUsageChanged( string packageIdent, long userCount );
+	///// <summary>
+	///// The backend is telling us that the number of users playing has changed
+	///// </summary>
+	//void PackageUsageChanged( string packageIdent, long userCount );
 
-	/// <summary>
-	/// Notifies that the number of favourites for the specified package has changed.
-	/// </summary>
-	void PackageFavouritesChanged( string packageIdent, long value );
+	///// <summary>
+	///// Notifies that the number of favourites for the specified package has changed.
+	///// </summary>
+	//void PackageFavouritesChanged( string packageIdent, long value );
 
 	/// <summary>
 	/// True if we want to force the cursor to be visible and swallow input.
