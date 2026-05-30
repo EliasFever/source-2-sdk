8c14cb7f161bfcb2277e0cad29caf30ba2441b56
Roman <romanplay@mail.ru>
2026-05-31 01:31:28 +0300

    welcome back the ketal error popup
    https://cdn.discordapp.com/attachments/1450076545153765512/1510410173272031323/image.png?ex=6a1cb697&is=6a1b6517&hm=69a32f187a00f4d1df8e1c009e7cfcdf5ba78387170f93abe7c2c77c7586829d&

--- game/addons/base/code/UISystem/DevUI/ExceptionNotification.cs
+++ game/addons/base/code/UISystem/DevUI/ExceptionNotification.cs
@@ -4,39 +4,39 @@ namespace Sandbox.UI.Dev;
 
 public class ExceptionNotification : Panel
 {
-    Label message;
-    RealTimeSince TimeSinceLastError;
-    Panel inner;
+	Label message;
+	RealTimeSince TimeSinceLastError;
+	Panel inner;
 
-    public ExceptionNotification()
-    {
-        inner = new Panel( this, "inner" );
+	public ExceptionNotification()
+	{
+		inner = new Panel( this, "inner" );
 
-        var img = inner.Add.Image( null, "icon-small" );
-        img.SetTexture( "tools/images/common/generic_hud_warning.png" );
-        img.Style.Width = 64;
-        img.Style.Height = 64;
+		var img = inner.Add.Image( null, "icon-small" );
+		img.SetTexture( "tools/images/common/generic_hud_warning.png" );
+		img.Style.Width = 64;
+		img.Style.Height = 64;
 
-        var column = new Panel( inner, "column" );
+		var column = new Panel( inner, "column" );
 
-        column.AddChild( new Label() { Text = "Code Error" } );
-        message = column.Add.Label( "Something went wrong! This is an exception notice!", "message" );
+		column.AddChild( new Label() { Text = "Code Error" } );
+		message = column.Add.Label( "Something went wrong! This is an exception notice!", "message" );
 
-        SetClass( "hidden", true );
-        TimeSinceLastError = 100;
-    }
+		SetClass( "hidden", true );
+		TimeSinceLastError = 100;
+	}
 
-    public override void Tick()
-    {
-        base.Tick();
+	public override void Tick()
+	{
+		base.Tick();
 
-        SetClass( "hidden", TimeSinceLastError > 8 );
-        SetClass( "fresh", TimeSinceLastError < 0.2f );
-    }
+		SetClass( "hidden", TimeSinceLastError > 8 );
+		SetClass( "fresh", TimeSinceLastError < 0.2f );
+	}
 
-    internal void OnException( LogEvent entry )
-    {
-        message.Text = entry.Message?.Split( '\n', System.StringSplitOptions.RemoveEmptyEntries ).FirstOrDefault()?.Trim() ?? "null";
-        TimeSinceLastError = 0;
-    }
+	internal void OnException( LogEvent entry )
+	{
+		message.Text = entry.Message?.Split( '\n', System.StringSplitOptions.RemoveEmptyEntries ).FirstOrDefault()?.Trim() ?? "null";
+		TimeSinceLastError = 0;
+	}
 }
