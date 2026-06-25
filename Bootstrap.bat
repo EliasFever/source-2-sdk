a0aeee385b9dd6fef097691c8a3f15f4188aa71e
Roman <romanplay@mail.ru>
2026-06-25 13:00:07 +0300

    add PAUSE to bootstrap.bat so you dont have to do the cmd trick for it to stay

--- Bootstrap.bat
+++ Bootstrap.bat
@@ -3,3 +3,4 @@
 dotnet run --project .\engine\Tools\SboxBuild\SboxBuild.csproj -- build --config Developer
 dotnet run --project .\engine\Tools\SboxBuild\SboxBuild.csproj -- build-shaders
 dotnet run --project .\engine\Tools\SboxBuild\SboxBuild.csproj -- build-content
+PAUSE
